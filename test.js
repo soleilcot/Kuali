30a1b81ef0dcf489d30a82c1dc1ee26ca3c30a41
Soleil <soleilcotterell@gmail.com>
2021-09-09 23:23:33 -0500

    Added comments and made the console output more specific.

--- test.js
+++ test.js
@@ -2,13 +2,14 @@
 Purpose: This code will facilitate reading from a local CSV file, parsing into
          JSON format and calling to a web service endpoint.
 */
-//We will use Papa Parse module to read CSV file into an array of JavaScript objects.
-
+//We use this module to facilitate parsing CSV
 const papa = require("papaparse")
+//We use this built in file system module to read local files
 const fs = require('fs')
 const file = fs.createReadStream('courses.csv')
-console.log(file)
+
+//Parse the CSV data we read from file into an array of native objects
 papa.parse(file,{header: true,complete: function(results){
-  console.log(results);
+  console.log(results.data);
 }
 });
